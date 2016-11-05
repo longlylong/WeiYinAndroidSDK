2f622a7066193d87755e609790d82cd4f04136ae
longlylong <86416817@qq.com>
2016-11-05 10:55:05 +0800

    1.4.5

--- app/src/main/java/com/example/king/weiyinsdk/MainActivity.java
+++ app/src/main/java/com/example/king/weiyinsdk/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.king.weiyinsdk;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
@@ -19,7 +18,6 @@ import com.weiyin.wysdk.basesdk.interfaces.WYLoadMoreListener;
 import com.weiyin.wysdk.basesdk.interfaces.WYPayOrderListener;
 import com.weiyin.wysdk.model.request.RequestStructDataBean;
 import com.weiyin.wysdk.util.SpUtils;
-import com.weiyin.wysdk.util.StatusBarUtil;
 import com.weiyin.wysdk.util.thread.MainThreadExecutor;
 
 import java.util.ArrayList;
@@ -169,55 +167,74 @@ public class MainActivity extends AppCompatActivity {
 
     private void addData() {
         //图片素材 必须是网络路径 宽高也是必要的
-        String frontCoverUrl = "http://image.weiyin.cc/719/185904/6da25ae9-c89c-410a-a437-8924102d1da1@1o_800w";//1334 x 1334
-        String flyleafHeadUrl = "http://image.weiyin.cc/719/185904/b6464d78-1ed9-4c4f-8fad-937a614d9893@1o_800w";//461 x 461
-        String backCoverUrl = "http://image.weiyin.cc/719/185904/9B104E17-FEB1-40D5-8921-51B515CD0156@1o_800w";//1334 x 1334
+        String frontCoverUrl = "http://image.weiyin.cc/719/185904/FD3DB404-B318-434A-9DE2-72FF157B3857@1o_800w";
+        String flyleafHeadUrl = "http://image.weiyin.cc/719/185904/b6464d78-1ed9-4c4f-8fad-937a614d9893@1o_200w";
+        String backCoverUrl = "http://image.weiyin.cc/719/185904/347B3767-12C6-4C60-844E-5012B1BF0F4E@1o_800w";
 
         //章节 微印代言人的图片
-        String photoUrl1 = "http://image.weiyin.cc/719/185904/f8c4624e-f371-491c-b260-01aabe395685@1o_800w";//1288 x 859
-        String photoUrl2 = "http://image.weiyin.cc/719/185904/b970b0fe-79f5-4305-bd1d-6d84768b2c77@1o_800w";//1288 x 859
-        String photoUrl3 = "http://image.weiyin.cc/719/185904/8bb5a60a-443b-4c4c-821c-cc821b48efb8@1o_800w";//1288 x 859
-        String photoUrl4 = "http://image.weiyin.cc/719/185904/8e427964-961e-44f9-9ded-28d9272e3041@1o_800w";//1288 x 859
-        String photoUrl5 = "http://image.weiyin.cc/719/185904/e99a1f5e-20b4-4c5f-a059-3d1fc6940965@1o_800w";//1288 x 859
-        String photoUrl6 = "http://image.weiyin.cc/719/185904/36b1bf8f-7a23-4bb6-9a81-7517078ded82@1o_800w";//1288 x 859
-        String photoUrl7 = "http://image.weiyin.cc/719/185904/1deda7c7-817f-4995-9975-80b7e9d0075a@1o_800w";//1288 x 859
-        String photoUrl8 = "http://image.weiyin.cc/719/185904/a3424258-9ead-4264-99d9-ab38ebec967e@1o_800w";//1288 x 859
-        String photoUrl9 = "http://image.weiyin.cc/719/185904/1deda7c7-817f-4995-9975-80b7e9d0075a@1o_800w";//1288 x 859
-        String photoUrl10 = "http://image.weiyin.cc/719/185904/14d5fb62-e71d-4768-8481-05083223ac4d@1o_800w";//1288 x 859
-        String photoUrl11 = "http://image.weiyin.cc/719/185904/6da25ae9-c89c-410a-a437-8924102d1da1@1o_800w";//1288 x 859
+        String photoUrl1 = "http://image.weiyin.cc/719/185904/f8c4624e-f371-491c-b260-01aabe395685@1o_800w";
+        String photoUrl2 = "http://image.weiyin.cc/719/185904/b970b0fe-79f5-4305-bd1d-6d84768b2c77@1o_800w";
+        String photoUrl3 = "http://image.weiyin.cc/719/185904/8bb5a60a-443b-4c4c-821c-cc821b48efb8@1o_800w";
+        String photoUrl4 = "http://image.weiyin.cc/719/185904/8e427964-961e-44f9-9ded-28d9272e3041@1o_800w";
+        String photoUrl5 = "http://image.weiyin.cc/719/185904/e99a1f5e-20b4-4c5f-a059-3d1fc6940965@1o_800w";
+        String photoUrl6 = "http://image.weiyin.cc/719/185904/36b1bf8f-7a23-4bb6-9a81-7517078ded82@1o_800w";
+        String photoUrl7 = "http://image.weiyin.cc/719/185904/1deda7c7-817f-4995-9975-80b7e9d0075a@1o_800w";
+        String photoUrl8 = "http://image.weiyin.cc/719/185904/a3424258-9ead-4264-99d9-ab38ebec967e@1o_800w";
+        String photoUrl9 = "http://image.weiyin.cc/719/185904/1deda7c7-817f-4995-9975-80b7e9d0075a@1o_800w";
+        String photoUrl10 = "http://image.weiyin.cc/719/185904/14d5fb62-e71d-4768-8481-05083223ac4d@1o_800w";
+        String photoUrl11 = "http://image.weiyin.cc/719/185904/6da25ae9-c89c-410a-a437-8924102d1da1@1o_800w";
 
-        //章节 画册简介 的图片
-        String photoUrl12 = "http://image.weiyin.cc/719/185904/557a28b6-a9fe-4978-a108-6b59ca13db10@1o_800w";//1288 x 859 瀑布流排版
-        String photoUrl13 = "http://image.weiyin.cc/719/185904/91cc8c4e-cff9-4ab0-beec-f9279408f059@1o_800w";//1288 x 859 拼图排版
-        String photoUrl14 = "http://image.weiyin.cc/719/185904/20D0D6E3-D1EB-439B-ADDC-310DF547BD27@1o_800w";//1288 x 859 通栏排版
-        String photoUrl15 = "http://image.weiyin.cc/719/185904/ee8efe5b-071a-4ced-8a2c-4dbc17291481@1o_800w";//1288 x 859 美感纸
-        String photoUrl16 = "http://image.weiyin.cc/719/185904/d36b0ad5-bb2d-4267-8e85-d9f8878a4c96@1o_800w";//1288 x 859 美感纸
-        String photoUrl17 = "http://image.weiyin.cc/719/185904/f035e4a2-5336-47f7-a823-ac69eae80f19@1o_800w";//1288 x 859 双铜对裱覆触感膜
-        String photoUrl18 = "http://image.weiyin.cc/719/185904/ca79d819-6ef4-47fa-9b2b-c4eecd6e6e8f@1o_800w";//1288 x 859 双铜对裱覆触感膜
-        String photoUrl19 = "http://image.weiyin.cc/719/185904/3ce2590a-6432-4bfe-ab74-0af8a338a918@1o_800w";//1288 x 859
-        String photoUrl20 = "http://image.weiyin.cc/719/185904/e96c26c8-4b50-40bf-9d04-4e16e99171e9@1o_800w";//1288 x 859 对裱装订方式：高档蝴蝶精装，无缝跨页，180度平铺，完美视觉效果
-        String photoUrl21 = "http://image.weiyin.cc/719/185904/b8bfb801-2c3d-4a3d-bcef-adc64e2aa961@1o_800w";//1288 x 859 普通精装装订方式：简胶装
-        String photoUrl22 = "http://image.weiyin.cc/719/185904/15b4be8d-45a4-4c0e-82ec-1a31350d5764@1o_800w";//1288 x 859
-        String photoUrl23 = "http://image.weiyin.cc/719/185904/7831b58e-35dd-4b8e-8ccd-120e9e8e7630@1o_800w";//1288 x 859 内配4个护角
-        String photoUrl24 = "http://image.weiyin.cc/719/185904/30f25df8-ee53-465f-ad07-64b4b57a219e@1o_800w";//1288 x 859 内配4个护角
-        String photoUrl25 = "http://image.weiyin.cc/719/185904/ee454d38-88d3-4959-8a3d-9afacf62c1e7@1o_800w";//1288 x 859 外有飞机盒包装，防撞防摔，保护书本
-        String photoUrl26 = "http://image.weiyin.cc/719/185904/44abdcba-86bc-43e8-a721-40327e2981cd@1o_800w";//1288 x 859 外有飞机盒包装，防撞防摔，保护书本
+        //章节：记录旧时光的照片书
+        String photoUrl12 = "http://image.weiyin.cc/719/185904/2e38af77-bf26-451a-859c-2a1383041d4d@1o_400w";
+        String photoUrl12Text = "纸质书尺寸为235*235mm，对比市场上的书大小一般都是A5（21.5cm*14cm），尺寸增加25%";
+        String photoUrl13 = "http://image.weiyin.cc/719/185904/557a28b6-a9fe-4978-a108-6b59ca13db10@1o_400w";
+        String photoUrl13Text = "瀑布流排版：按照片顺序铺陈排版，能完整显示全部照片";
+        String photoUrl14 = "http://image.weiyin.cc/719/185904/91cc8c4e-cff9-4ab0-beec-f9279408f059@1o_400w";
+        String photoUrl14Text = "拼图排版：根据照片比例匹配系统模板，自动拼图排版";
+        String photoUrl15 = "http://image.weiyin.cc/719/185904/8b35f243-f887-4b41-a4eb-8b52ece6e7ca@1o_400w";
+        String photoUrl15Text = "内页：美感极致。纸色自然柔和，独具专利的先进涂布工艺，独特的表面质感，层次感强，是高档画册等高精印品的专业之选。";
+        String photoUrl16 = "http://image.weiyin.cc/719/185904/921cc23d-293a-4939-bda1-93c5ee8aa13f@1o_400w";
+        String photoUrl16Text = "内页：120克美感纸";
+        String photoUrl17 = "http://image.weiyin.cc/719/185904/3ce2590a-6432-4bfe-ab74-0af8a338a918@1o_400w";
+        String photoUrl17Text = "印刷：使用惠普indigo 10000高清顶级印刷。它采用HP Indigo液体电子油墨技术和独特的数字胶印工艺，能印刷出清晰的线条、绚丽夺目的图像和插图；印刷质量最高，堪比甚至超过胶印；还原性强且画质细腻。最主要，全国20多台，我们有其中一台";
+        String photoUrl18 = "http://image.weiyin.cc/719/185904/7831b58e-35dd-4b8e-8ccd-120e9e8e7630@1o_400w";
+        String photoUrl18Text = "内配4个护角";
+        String photoUrl19 = "http://image.weiyin.cc/719/185904/30f25df8-ee53-465f-ad07-64b4b57a219e@1o_400w";
+        String photoUrl19Text = photoUrl18Text;
 
-        //章节 纸质书实物照 的图片
-        String photoUrl27 = "http://image.weiyin.cc/719/185904/4e684ef3-d54e-4d9f-9495-307f930dd932@1o_800w";//1288 x 859
-        String photoUrl28 = "http://image.weiyin.cc/719/185904/68b88d25-79ed-45c6-9cf9-b3aa3d40d3a3@1o_800w";//1288 x 859
-        String photoUrl29 = "http://image.weiyin.cc/719/185904/e70c0905-1150-4732-b5cb-ea75f4fe02f3@1o_800w";//1288 x 859
-        String photoUrl30 = "http://image.weiyin.cc/719/185904/7b505f59-f6c7-4c9c-8dba-6a0bcaca47cc@1o_800w";//1288 x 859
-        String photoUrl31 = "http://image.weiyin.cc/719/185904/a5109197-e90c-43e9-aadf-f7e42aa8a56b@1o_800w";//1288 x 859
-        String photoUrl32 = "http://image.weiyin.cc/719/185904/22f59dbe-3ba1-4ff9-9121-24383de914f1@1o_800w";//1288 x 859
-        String photoUrl33 = "http://image.weiyin.cc/719/185904/cb977f37-e0ac-408b-97fe-573eb1f3a5c5@1o_800w";//1288 x 859
-        String photoUrl34 = "http://image.weiyin.cc/719/185904/f75eb1d4-3059-4737-92a0-eae1535a0c9e@1o_800w";//1288 x 859
-        String photoUrl35 = "http://image.weiyin.cc/719/185904/6b871dd7-e121-4d41-baf8-d024864cc5a4@1o_800w";//1288 x 859
-        String photoUrl36 = "http://image.weiyin.cc/719/185904/0ff2356f-e0b5-41b0-a36d-c39a4c546ed8@1o_800w";//1288 x 859
-        String photoUrl37 = "http://image.weiyin.cc/719/185904/b3f71a8a-710b-4de8-a304-7f5906dfbf5b@1o_800w";//1288 x 859
-        String photoUrl38 = "http://image.weiyin.cc/719/185904/921cc23d-293a-4939-bda1-93c5ee8aa13f@1o_800w";//1288 x 859
-        String photoUrl39 = "http://image.weiyin.cc/719/185904/d621c028-9267-47ae-b6b5-477d8d4060ef@1o_800w";//1288 x 859
-        String photoUrl40 = "http://image.weiyin.cc/719/185904/ccb33e32-6ce0-4ae8-b7c4-9f637037dc69@1o_800w";//1288 x 859
+        //章节：文艺的照片卡片
+        String photoUrl27 = "http://image.weiyin.cc/719/185904/320c0493-08bc-43bb-a48c-40ef0d50ba27@1o_400w";
+        String photoUrl27Text = "卡片材质：280克珠光纸";
+        String photoUrl28 = "http://image.weiyin.cc/719/185904/679b4e03-36d3-4053-8a67-8d478c9eeb17@1o_400w";
+        String photoUrl28Text = "尺寸：10.75cm*7.25cm";
+        String photoUrl29 = "http://image.weiyin.cc/719/185904/305fabe6-020c-461c-9564-b15d1aada91c@1o_400w";
+        String photoUrl30 = "http://image.weiyin.cc/719/185904/6e431524-bbbc-47b9-a998-4b35749f5579@1o_400w";
+
+        //章节：怀旧的照片冲印
+        String photoUrl39 = "http://image.weiyin.cc/719/185904/c220467d-77f1-4f1d-929d-80f2ba8b93c6@1o_400w";
+        String photoUrl39Text = "300克双铜纸，画质细腻，呈现生动逼真的效果";
+        String photoUrl40 = "http://image.weiyin.cc/719/185904/c03b80b7-c1a0-4d61-b387-3b8b6a9429b0@1o_400w";
+        String photoUrl40Text = "高品质，双面过膜，正面过压纹膜，背面过哑膜。防水防污防氧化，平整不卷边";
+        String photoUrl41 = "http://image.weiyin.cc/719/185904/fb5076c7-803c-4543-b885-ac17bf856dfc@1o_400w";
+        String photoUrl41Text = "大6寸（4D）：150mmX114mm";
+        String photoUrl42 = "http://image.weiyin.cc/719/185904/948ad9cb-21cf-474f-832f-9223ae7d9eaa@1o_400w";
+        String photoUrl42Text = "惠普indigo10000高清顶级印刷，还原照片颜色和细节。采用惠普环保电子油墨，无银盐，呵护家人健康";
+
+        //章节：精美照片定制台历
+        String photoUrl43 = "http://image.weiyin.cc/719/185904/104a0dd8-81b6-4a10-92b2-c98b37996acf@1o_400w";
+        String photoUrl43Text = "尺寸：A5大小（横版210mmX148mm）";
+        String photoUrl44 = "http://image.weiyin.cc/719/185904/8784c33e-8142-4172-b53d-b0c5f02991ca@1o_400w";
+        String photoUrl44Text = "内页：采用250克铜版纸印制，纸张平滑有光泽";
+        String photoUrl45 = "http://image.weiyin.cc/719/185904/462f1c85-e4d2-4092-8e8d-5956431a3404@1o_400w";
+        String photoUrl45Text = "装订：采用白色双线铁圈装订，360度灵活翻阅，牢固耐用";
+        String photoUrl46 = "http://image.weiyin.cc/719/185904/4d96f47c-2def-4548-b354-5af45e4a2107@1o_400w";
+        String photoUrl46Text = "底座：采用超厚牛皮纸板为支撑，可以平稳的立在桌面上";
+        String photoUrl47 = "http://image.weiyin.cc/719/185904/17a8fcd1-0de5-43cf-ad5e-9496269ac7d6@1o_400w";
+        String photoUrl47Text = "封面";
+        String photoUrl48 = "http://image.weiyin.cc/719/185904/46877022-4ef3-4e53-addf-ef71624919e7@1o_400w";
+        String photoUrl48Text = "内页1图，2图随机排版";
+        String photoUrl49 = "http://image.weiyin.cc/719/185904/a7860f64-7b53-48ff-afb0-541f55e14a75@1o_400w";
+        String photoUrl49Text = "背面可记事";
 
         //拍摄时间,由于是网络图片就自定义了一个时间
         long originalTime = System.currentTimeMillis() / 1000;
@@ -244,39 +261,37 @@ public class MainActivity extends AppCompatActivity {
         WYSdk.getInstance().addPhotoBlock("", photoUrl10, photoUrl10, originalTime, 1289, 806);
         WYSdk.getInstance().addPhotoBlock("", photoUrl11, photoUrl11, originalTime, 1289, 806);
 
-        WYSdk.getInstance().addChapterBlock("画册简介", "");
-        WYSdk.getInstance().addPhotoBlock("瀑布流排版", photoUrl12, photoUrl12, originalTime, 1289, 806);
-        WYSdk.getInstance().addPhotoBlock("拼图排版", photoUrl13, photoUrl13, originalTime, 1289, 806);
-        WYSdk.getInstance().addPhotoBlock("通栏排版", photoUrl14, photoUrl14, originalTime, 1289, 806);
-        WYSdk.getInstance().addPhotoBlock("美感纸", photoUrl15, photoUrl15, originalTime, 1289, 806);
-        WYSdk.getInstance().addPhotoBlock("美感纸", photoUrl16, photoUrl16, originalTime, 1289, 806);
-        WYSdk.getInstance().addPhotoBlock("双铜对裱覆触感膜", photoUrl17, photoUrl17, originalTime, 1289, 806);
-        WYSdk.getInstance().addPhotoBlock("双铜对裱覆触感膜", photoUrl18, photoUrl18, originalTime, 1289, 806);
-        WYSdk.getInstance().addPhotoBlock("", photoUrl19, photoUrl19, originalTime, 1289, 806);
-        WYSdk.getInstance().addPhotoBlock("对裱装订方式：高档蝴蝶精装，无缝跨页，180度平铺，完美视觉效果", photoUrl20, photoUrl20, originalTime, 1289, 806);
-        WYSdk.getInstance().addPhotoBlock("普通精装装订方式：简胶装", photoUrl21, photoUrl21, originalTime, 1289, 806);
-        WYSdk.getInstance().addPhotoBlock("", photoUrl22, photoUrl22, originalTime, 1289, 806);
-        WYSdk.getInstance().addPhotoBlock("内配4个护角", photoUrl23, photoUrl23, originalTime, 1289, 806);
-        WYSdk.getInstance().addPhotoBlock("内配4个护角", photoUrl24, photoUrl24, originalTime, 1289, 806);
-        WYSdk.getInstance().addPhotoBlock("外有飞机盒包装，防撞防摔，保护书本", photoUrl25, photoUrl25, originalTime, 1289, 806);
-        WYSdk.getInstance().addPhotoBlock("外有飞机盒包装，防撞防摔，保护书本", photoUrl26, photoUrl26, originalTime, 1289, 806);
+        WYSdk.getInstance().addChapterBlock("记录旧时光的照片书", "");
+        WYSdk.getInstance().addPhotoBlock(photoUrl12Text, photoUrl12, photoUrl12, originalTime, 1289, 806);
+        WYSdk.getInstance().addPhotoBlock(photoUrl13Text, photoUrl13, photoUrl13, originalTime, 1289, 806);
+        WYSdk.getInstance().addPhotoBlock(photoUrl14Text, photoUrl14, photoUrl14, originalTime, 1289, 806);
+        WYSdk.getInstance().addPhotoBlock(photoUrl15Text, photoUrl15, photoUrl15, originalTime, 1289, 806);
+        WYSdk.getInstance().addPhotoBlock(photoUrl16Text, photoUrl16, photoUrl16, originalTime, 1289, 806);
+        WYSdk.getInstance().addPhotoBlock(photoUrl17Text, photoUrl17, photoUrl17, originalTime, 1289, 806);
+        WYSdk.getInstance().addPhotoBlock(photoUrl18Text, photoUrl18, photoUrl18, originalTime, 1289, 806);
+        WYSdk.getInstance().addPhotoBlock(photoUrl19Text, photoUrl19, photoUrl19, originalTime, 1289, 806);
 
-        WYSdk.getInstance().addChapterBlock("纸质书实物照", "");
-        WYSdk.getInstance().addPhotoBlock("", photoUrl27, photoUrl27, originalTime, 1289, 806);
-        WYSdk.getInstance().addPhotoBlock("", photoUrl28, photoUrl28, originalTime, 1289, 806);
+        WYSdk.getInstance().addChapterBlock("文艺的照片卡片", "");
+
+        WYSdk.getInstance().addPhotoBlock(photoUrl27Text, photoUrl27, photoUrl27, originalTime, 1289, 806);
+        WYSdk.getInstance().addPhotoBlock(photoUrl28Text, photoUrl28, photoUrl28, originalTime, 1289, 806);
         WYSdk.getInstance().addPhotoBlock("", photoUrl29, photoUrl29, originalTime, 1289, 806);
         WYSdk.getInstance().addPhotoBlock("", photoUrl30, photoUrl30, originalTime, 1289, 806);
-        WYSdk.getInstance().addPhotoBlock("", photoUrl31, photoUrl31, originalTime, 1289, 806);
-        WYSdk.getInstance().addPhotoBlock("", photoUrl32, photoUrl32, originalTime, 1289, 806);
-        WYSdk.getInstance().addPhotoBlock("", photoUrl33, photoUrl33, originalTime, 1289, 806);
-        WYSdk.getInstance().addPhotoBlock("", photoUrl34, photoUrl34, originalTime, 1289, 806);
-        WYSdk.getInstance().addPhotoBlock("", photoUrl35, photoUrl35, originalTime, 1289, 806);
-        WYSdk.getInstance().addPhotoBlock("", photoUrl36, photoUrl36, originalTime, 1289, 806);
-        WYSdk.getInstance().addPhotoBlock("", photoUrl37, photoUrl37, originalTime, 1289, 806);
-        WYSdk.getInstance().addPhotoBlock("", photoUrl38, photoUrl38, originalTime, 1289, 806);
-        WYSdk.getInstance().addPhotoBlock("", photoUrl39, photoUrl39, originalTime, 1289, 806);
-        WYSdk.getInstance().addPhotoBlock("", photoUrl40, photoUrl40, originalTime, 1289, 806);
 
+        WYSdk.getInstance().addChapterBlock("怀旧的照片冲印", "");
+        WYSdk.getInstance().addPhotoBlock(photoUrl39Text, photoUrl39, photoUrl39, originalTime, 1289, 806);
+        WYSdk.getInstance().addPhotoBlock(photoUrl40Text, photoUrl40, photoUrl40, originalTime, 1289, 806);
+        WYSdk.getInstance().addPhotoBlock(photoUrl41Text, photoUrl41, photoUrl41, originalTime, 1289, 806);
+        WYSdk.getInstance().addPhotoBlock(photoUrl42Text, photoUrl42, photoUrl42, originalTime, 1289, 806);
+
+        WYSdk.getInstance().addChapterBlock("精美照片定制台历", "");
+        WYSdk.getInstance().addPhotoBlock(photoUrl43Text, photoUrl43, photoUrl43, originalTime, 1289, 806);
+        WYSdk.getInstance().addPhotoBlock(photoUrl44Text, photoUrl44, photoUrl44, originalTime, 1289, 806);
+        WYSdk.getInstance().addPhotoBlock(photoUrl45Text, photoUrl45, photoUrl45, originalTime, 1289, 806);
+        WYSdk.getInstance().addPhotoBlock(photoUrl46Text, photoUrl46, photoUrl46, originalTime, 1289, 806);
+        WYSdk.getInstance().addPhotoBlock(photoUrl47Text, photoUrl47, photoUrl47, originalTime, 1289, 806);
+        WYSdk.getInstance().addPhotoBlock(photoUrl48Text, photoUrl48, photoUrl48, originalTime, 1289, 806);
+        WYSdk.getInstance().addPhotoBlock(photoUrl49Text, photoUrl49, photoUrl49, originalTime, 1289, 806);
 
 //        WYSdk.getInstance().addTextBlock("对裱精装\n\n" +
 //                "封面：高档硬壳封面  双铜纸覆触感膜\n\n" +
